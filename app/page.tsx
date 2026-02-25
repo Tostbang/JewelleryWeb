17540f0c7784ef2dbb0f261c6cff0f7b2bf6623e
asemamr <asimamr8@gmail.com>
2026-02-25 16:24:39 +0300

    redeployment

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ import { SupportForm } from "@/components/support/SupportForm";
 export default function Page() {
   return (
     <main>
-      {/* test */}
       <NavBar />
       <Hero />
       <Logos />
